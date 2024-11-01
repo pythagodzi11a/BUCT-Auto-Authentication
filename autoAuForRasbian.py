b2d6371eaf4709a0df75e4dbb137959962e31c85
Pythagodzilla <57862280+pythagodzi11a@users.noreply.github.com>
2024-11-01 14:47:30 +0800

    Update autoAuForRasbian.py
    nonono

--- autoAuForRasbian.py
+++ autoAuForRasbian.py
@@ -7,8 +7,8 @@ browser.get("https://tree.buct.edu.cn/index_20.html")
 
 try:
     browser.implicitly_wait(10)
-    browser.find_element(By.XPATH,'//*[@id="username"]').send_keys('2024050276')
-    browser.find_element(By.XPATH,'//*[@id="password"]').send_keys('@Jtbx2mtblj')
+    browser.find_element(By.XPATH,'//*[@id="username"]').send_keys('账号')
+    browser.find_element(By.XPATH,'//*[@id="password"]').send_keys('密码')
     browser.find_element(By.XPATH,'//*[@id="login-account"]').click()
 except Exception:
     print('Authentication finish!')
